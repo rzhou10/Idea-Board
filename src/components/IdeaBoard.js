d34661afb2a9b69e9203e7a00a645de24bd68afd
rzhou10 <rzhou10@gmail.com>
2019-01-28 14:09:38 -0500

    Added functionalities for buttons/css updates

--- src/components/IdeaBoard.js
+++ src/components/IdeaBoard.js
@@ -12,34 +12,44 @@ class IdeaBoard extends Component {
     }
 
     componentDidMount(){
-        
+        axios.get(`http://localhost:4000/`).then((data) =>
+            this.setState( { ideas: data } )
+        )
     }
 
     handleChange = e => {
         this.setState({ [e.target.name]: e.target.value })
     }
 
-    addIdea(){
+    addIdea = () => {
         const reqBody = {
             "idea": this.state.idea,
             "description": this.state.description,
         }
 
-        let sliced = this.state.ideas.slice()
-        axios.post(`http://localhost:4000/add`).then(
+        let sliced = this.state.ideas.slice();
+        axios.post(`http://localhost:4000/add`, reqBody).then(
             () => {
                 sliced.push(reqBody);
                 this.setState({ideas: sliced})
             }
+        );
+    }
+
+    removeIdea = (props) => {
+        let sliced = this.state.ideas.slice();
+        axios.delete(`http://localhost:4000/` + props._id).then(
+            () => {
+                let filtered = sliced.filter((val) => {
+                    return val !== props;
+                });
+                this.setState({ideas: filtered});
+            }
         )
     }
 
-    removeIdea(){
-        
-    }
-
-    editIdea(){
-
+    editIdea = (props) => {
+        axios.patch(`http://localhost:4000/` + props._id);
     }
 
     render(){
@@ -53,18 +63,17 @@ class IdeaBoard extends Component {
                         <label>
                             Description: <input type="text" value={this.state.description} name="description" onChange={this.handleChange} required/>
                         </label>
-                        <input type="submit" />
+                        <input type="submit" value="Add Idea"/>
                     </form>
                 </div>
                 <div className="ideas">
-                    {this.state.ideas.map(
-                        (idea) => {
-                            <div className="specificIdea">
-                                <div className="changeStuff">{this.editIdea}{this.removeIdea}</div>
-                                <p>{idea.idea}</p>
-                                <p>{idea.description}</p>
-                            </div>
-                        }
+                    {this.state.ideas.map((idea) => 
+                        <div key={idea._id} className="specificIdea">
+                            <div className="changeStuff"><button onClick={this.editIdea(idea)}></button> <button onClick={this.removeIdea(idea)}></button></div>
+                            <p>{idea.idea}</p>
+                            <p>{idea.description}</p>
+                        </div>
+                        
                     )}
                 </div>
             </div>
